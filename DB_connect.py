92c4b1bc97831560672603c08165d3cfdb951c46
Nick Oliger <naoliger@edu.hse.ru>
2022-07-06 17:29:01 +0300

    Final changer for 06.07 17:30

--- DB_connect.py
+++ DB_connect.py
@@ -2,6 +2,7 @@ import psycopg2
 import time
 from sqlite3 import OperationalError
 
+
 def create_connection(db_name, db_user, db_password, db_host, db_port):
     connection = None
     try:
@@ -18,9 +19,7 @@ def create_connection(db_name, db_user, db_password, db_host, db_port):
     return connection
 
 
-connection = create_connection(
-    "postgres", "nickoliger", "", "127.0.0.1", "5432"
-)
+connection = create_connection("postgres", "nickoliger", "", "127.0.0.1", "5432")
 
 
 def execute_query(connection, query):
@@ -43,7 +42,6 @@ def execute_read_query(connection, query):
     except Error as e:
         print(f"The error '{e}' occurred")
 
-
 create_user_info = """
 CREATE TABLE IF NOT EXISTS user_info
 (
@@ -51,10 +49,10 @@ CREATE TABLE IF NOT EXISTS user_info
     last_name          VARCHAR,
     middle_name        VARCHAR,
     user_id            INTEGER UNIQUE,
-    phone_number       VARCHAR(11),
+    phone_number       VARCHAR,
     age                INTEGER,
     citizenship        INTEGER,
-    current_stage      INTEGER,
+    current_stage      VARCHAR,
     track              INTEGER,
     utc_update_time    INTEGER
 );
@@ -87,51 +85,46 @@ execute_query(connection, create_user_info)
 
 
 users_TEST = [
-    ("Vasily", "Petrov", "Sergeevich", 79091234567, 1),
-    ("Anton", "Romanov", "Alexandrovich", 79993827117, 2),
-    ("Semyon", "Skvortsov", "Nikolayevich", 79631228923, 3)
+    ("Vasily", "Petrov", "Sergeevich", "79091234567", 1),
+    ("Anton", "Romanov", "Alexandrovich", "79993827117", 2),
+    ("Semyon", "Skvortsov", "Nikolayevich", "79631228923", 3),
 ]
 
 user_records_TEST = ", ".join(["%s"] * len(users_TEST))
 
-insert_query = (
-    f"INSERT INTO user_info (first_name, last_name, middle_name, phone_number, track) VALUES {user_records_TEST}"
-)
+insert_query = f"INSERT INTO user_info (first_name, last_name, middle_name, phone_number, track) VALUES {user_records_TEST}"
 
-#connection.autocommit = True
-#cursor = connection.cursor()
-#cursor.execute(insert_query, users_TEST)
+# connection.autocommit = True
+# cursor = connection.cursor()
+# cursor.execute(insert_query, users_TEST)
 
 
 select_tracks = """
 SELECT
   last_name,
   first_name,
-  track,
-  phone_number
+  middle_name,
+  phone_number,
+  current_stage
 FROM
   user_info
 """
 
-tracks = execute_read_query(
-    connection, select_tracks
-)
+tracks = execute_read_query(connection, select_tracks)
 
 for track in tracks:
-    if track[2]==1:
+    if track[2] == 1:
         pass
-    elif track[2]==2:
+    elif track[2] == 2:
         pass
-    elif track[2]==3:
+    elif track[2] == 3:
         pass
 
-#---Testing section---
+# ---Testing section---
 TESTER_COUNT = 1
 print("Iteration #", TESTER_COUNT)
-tracks = execute_read_query(
-    connection, select_tracks
-)
+tracks = execute_read_query(connection, select_tracks)
 for track in tracks:
     print(track)
 print("-----------------------------")
-#---------------------
+# ---------------------
